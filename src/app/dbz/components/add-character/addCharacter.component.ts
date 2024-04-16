7351a87e8b3afd52c9777aa981b44055abdc24e5
lrcangular <leticia.ribeiro@2shapes.com>
2024-04-16 14:19:10 +0200

    add_subModulos2

--- src/app/dbz/components/add-character/addCharacter.component.ts
+++ src/app/dbz/components/add-character/addCharacter.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dbz-add-character',
+  selector: 'dbz-add-character',
   templateUrl: './addCharacter.component.html',
   styleUrls: ['./addCharacter.component.css']
 })
